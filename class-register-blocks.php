db029986bfb6f2c2615eee7a206396febafff5fe
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2018-02-27 14:19:22 +0530

    Register gutenberg samle block

--- class-register-blocks.php
+++ class-register-blocks.php
@@ -20,8 +20,19 @@ class Register_Blocks {
 	 * Initialize Block.
 	 */
 	protected function init() {
+		add_action( 'init', array( $this, 'register_sample_block' ) );
+	}
 
-		// add_action( 'enqueue_block_editor_assets', array( $this, 'register_sample_block' ) );
+	/**
+	 * Register gutenberg lyrics block
+	 */
+	public function register_sample_block() {
+		wp_enqueue_script(
+			'gutenberg-sample-block',
+			RT_BLOCKS_DIR_URL . 'blocks/build/build.js',
+			array( 'wp-blocks', 'wp-i18n', 'wp-element' ),
+			RT_BLOCKS_DIR_URL
+		);
 	}
 }
 
